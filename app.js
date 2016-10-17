bfd17c3c01ccf23a841cd383982df6a51e6e9758
Birkan Duman <birkan@portos.com.tr>
2016-10-17 20:29:33 +0300

    more templates

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.controller('MainController', function($scope,DbTunesQuestionService){
 	console.log('Hello angular!');
 	DbTunesQuestionService.getAlbumSongQuestion()
 	.then(function(data){
-		//TODO populate question, answers and correct answer from song 
+		//serviste hazirlanan question objesi
 		$scope.question1 = data
 	}, function(err){
 		//TODO: handle error
